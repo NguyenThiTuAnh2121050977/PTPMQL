842064f1113a3e58138b4ed7d1c3c2f05e8e8865
“NguyenThiTuAnh2121050977 <tuanhnguyen7102003@gmail.com>
2024-09-20 16:22:55 +0700

    commit note

--- DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd7553a1c1ec6739f337f58f0d99a3c104788fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1eeae1a209b584a6274c471914c33cf9a178c94")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
